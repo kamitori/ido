86844062d2f939b9a7119bd93e6446a67d1dca05
Trí <nmtri44@gmail.com>
2017-10-31 16:51:14 +0700

    version 1

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Video;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -12,15 +12,9 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     public function getVideo(){
-    	// $html =  getList('https://aidol.asia/idol/page/1/');
-    	// $list_video = extractList($html);
-    	// foreach ($list_video as $key => $link) {
-    	// 	# code...
-    	// }
-    	$link = 'https://aidol.asia/idol/lcdv/6054-lcdv-40317-shizuka-nakamura-60f.html';
-    	$content = getLink($link);
-    	$video = extractContent($content);
-    	$video['link'] = $link;
-    	dd($video);
+        $i = 1;
+        $html =  getList('https://aidol.asia/idol/');
+        $list_video = extractList($html);
+        dd($list_video);
     }
 }
--- app/ultility.php
+++ app/ultility.php
@@ -102,9 +102,50 @@ function extractContent($html){
   $html_result = new \DOMXPath($html);
 
   $arrReturn['name'] = $html_result->query('//meta[@property="og:title"]/attribute::content')->item(0)->nodeValue;
-  // $arrReturn = [];
-  // foreach ($lists as $k => $item) {
-  // 	$arrReturn[] = $item->nodeValue;
-  // }
+  $arrReturn['image'] = 'https://aidol.asia/'.$html_result->query('//img[@itemprop="photo"]/attribute::src')->item(0)->nodeValue;
+  $download1 = $html_result->query('//span[@id="full442"]/a')->item(0)->nodeValue;
+  $arrReturn['download1'] = getLinkFollow($download1);
+  
+  $download2 = $html_result->query('//span[@id="full443"]/a')->item(0)->nodeValue;
+  $arrReturn['download2'] = getLinkFollow($download2);
+
+  $preview = $html_result->query('//span[@itemprop="thumbnail"]/a/img/attribute::src')->item(0)->nodeValue;
+
+  $arrReturn['preview']   = str_replace('/th/', '/i/', $preview);
+
+  $code     = $html_result->query('//span[@itemprop="episodeNumber"]')->item(0)->nodeValue;
+  $size     = $html_result->query('//span[@itemprop="contentSize"]')->item(0)->nodeValue;
+  $time     = $html_result->query('//span[@itemprop="duration"]')->item(0)->nodeValue;
+  $file     = $html_result->query('//span[@itemprop="encodingFormat"]')->item(0)->nodeValue;
+  $released = $html_result->query('//span[@itemprop="dateCreated"]/a')->item(0)->nodeValue;
+  $width    = $html_result->query('//span[@itemprop="videoFrameSize"]')->item(0)->nodeValue;
+  $studio   = '';
+  $studio_a = $html_result->query('//span[@itemprop="keywords"]/span/a');
+  foreach ($studio_a as $key => $value) {
+    if($key==0){
+      $studio   .= $value->nodeValue;
+    }else{
+      $studio   .= ','.$value->nodeValue;
+    }
+  }
+  $actors   = '';
+
+  $actors_a = $html_result->query('//span[@itemprop="actor"]/a');
+  foreach ($actors_a as $key => $value) {
+    if($key==0){
+      $actors   .= $value->nodeValue;
+    }else{
+      $actors   .= ','.$value->nodeValue;
+    }
+  }
+
+  $arrReturn['code']      =  $code;
+  $arrReturn['size']      =  $size;
+  $arrReturn['time']      =  $time;
+  $arrReturn['file']      =  $file;
+  $arrReturn['released']  =  $released;
+  $arrReturn['width']     =  $width;
+  $arrReturn['studio']    =  $studio;
+  $arrReturn['actors']    =  $actors;
   return $arrReturn;
 }
