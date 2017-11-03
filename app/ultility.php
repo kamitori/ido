700a1deb1c90be74b3d2b34f477139d2767c37b6
Trí <nmtri44@gmail.com>
2017-11-03 10:55:31 +0700

    update get video

--- app/ultility.php
+++ app/ultility.php
@@ -1,4 +1,15 @@
 <?php
+define('ARR_HEADER', 
+[
+  ['User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'],
+  // ['User-Agent: Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36'],
+  // ['User-Agent: Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25'],
+  // ['User-Agent: Mozilla/5.0 (MSIE 9.0; Windows NT 6.1; Trident/5.0)'],
+  // ['User-Agent: Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14'],
+  // ['User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:19.0) Gecko/20100101 Firefox/19.0'],
+  // ['User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:33.0) Gecko/20100101 Firefox/33.0'],
+  // ['User-Agent: Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920)'],
+]);
 
 function pr($array) {
 	echo "<pre>";
@@ -31,9 +42,7 @@ function getList($link){
   curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
   curl_setopt($ch, CURLOPT_ENCODING, 'UTF-8');
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-      'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:19.0) Gecko/20100101 Firefox/19.0',
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'));
+  curl_setopt($ch, CURLOPT_HTTPHEADER,ARR_HEADER[array_rand(ARR_HEADER)]);
   $response = curl_exec($ch);
   curl_close($ch);
   $d = new \DOMDocument();
@@ -57,9 +66,7 @@ function getLink($link){
   curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
   curl_setopt($ch, CURLOPT_ENCODING, 'UTF-8');
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-      'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:19.0) Gecko/20100101 Firefox/19.0',
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'));
+  curl_setopt($ch, CURLOPT_HTTPHEADER,ARR_HEADER[array_rand(ARR_HEADER)]);
   $response = curl_exec($ch);
   curl_close($ch);
   $d = new \DOMDocument();
@@ -74,9 +81,7 @@ function getLinkFollow($link){
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
   curl_setopt($ch, CURLOPT_HEADER, TRUE);
   curl_setopt($ch, CURLOPT_ENCODING, 'UTF-8');
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-      'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:19.0) Gecko/20100101 Firefox/19.0',
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'));
+  curl_setopt($ch, CURLOPT_HTTPHEADER,ARR_HEADER[array_rand(ARR_HEADER)]);
   $response = curl_exec($ch);
   curl_close($ch);
   $header = get_headers_from_curl_response($response);
